4c26d4f6dda52e3c6df0b39567e73106d044db2e
ArtemNA <artem.nevmerzhytskyi@gmail.com>
2023-04-10 18:02:21 +0300

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from "@nestjs/common";
+import axios from "axios";
 
 @Controller()
 export class AppController {
+  @Get('proxy')
+  async proxy(@Req() req, @Res() res, @Query('url') url: string) {
+    try {
+      const response = await axios.get(url, {
+        responseType: 'stream'
+      });
+
+      response.data.pipe(res);
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(500);
+    }
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import {
-  MiddlewareConsumer,
   Module,
-  NestModule,
-  RequestMethod,
 } from '@nestjs/common';
 
 import { AppController } from './app.controller';
@@ -10,24 +7,11 @@ import { AppService } from './app.service';
 import { UserAssessmentsModule } from './user-assessments/user-assessments.module';
 import { UsersModule } from './users/users.module';
 import { LoginModule } from './login/login.module';
-import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
   imports: [UserAssessmentsModule, UsersModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
-    consumer
-      .apply(AuthMiddleware)
-      .exclude({
-        path: 'login',
-        method: RequestMethod.ALL,
-      })
-      .forRoutes({
-        path: '/*',
-        method: RequestMethod.ALL,
-      });
-  }
+export class AppModule {
 }
